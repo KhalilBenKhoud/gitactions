d4a63381d65e9338f17a7fc54cade3824431b857
KhalilBenKhoud <khalil.b.khoud1998@gmail.com>
2025-07-23 19:28:26 +0100

    added caching depedencies

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -16,6 +16,6 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('helparea')).toBeInTheDocument();
+    expect(screen.getByTestId('help-area')).toBeInTheDocument();
   });
 });
